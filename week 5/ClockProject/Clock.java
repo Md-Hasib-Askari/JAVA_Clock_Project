6f165475dc305c1b26940ff5c384bed1177ced8e
MDHASIBASKARI <mdhsbaskari@gmail.com>
2023-02-23 16:32:21 +0600

    Improved Code

--- week 5/ClockProject/Clock.java
+++ week 5/ClockProject/Clock.java
@@ -16,18 +16,12 @@ public class Clock {
         this.seconds = (seconds % 3600) % 60;
     }
     public Clock(int hours, int minutes, int seconds) {
-
         this.hours = hours;
         this.minutes = minutes;
         this.seconds = seconds;
 
         int currentSeconds = getInSecond(this);
-        this.hours = (currentSeconds / 3600);
-        currentSeconds -= this.hours*3600;
-        this.hours %= 24;
-        this.minutes = currentSeconds / 60;
-        currentSeconds -= this.minutes*60;
-        this.seconds = currentSeconds;
+        calculateTime(currentSeconds, this);
 
     }
 
@@ -64,48 +58,37 @@ public class Clock {
 
     public void addClock(Clock obj) {
          int currentSeconds = (getInSecond(this) + getInSecond(obj));
-         this.hours = (currentSeconds / 3600);
-         currentSeconds -= this.hours*3600;
-        this.hours %= 24;
-        this.minutes = currentSeconds / 60;
-         currentSeconds -= this.minutes*60;
-         this.seconds = currentSeconds;
+         calculateTime(currentSeconds, this);
     }
 
     public Clock subtractClock(Clock obj) {
         Clock newObj = new Clock(this.hours, this.minutes, this.seconds);
         int currentSeconds = Math.abs(getInSecond(newObj) - getInSecond(obj));
-        newObj.setHours(currentSeconds / 3600);
-        currentSeconds -= newObj.getHours()*3600;
-        newObj.setHours(getHours() % 24);
-        newObj.setMinutes(currentSeconds / 60);
-        currentSeconds -= newObj.getMinutes()*60;
-        newObj.setSeconds(currentSeconds);
+        calculateTime(currentSeconds, newObj);
         return newObj;
     }
 
     public void tick() {
         int currentSeconds = getInSecond(this) + 1;
-        this.hours = (currentSeconds / 3600);
-        currentSeconds -= this.hours*3600;
-        this.hours %= 24;
-        this.minutes = currentSeconds / 60;
-        currentSeconds -= this.minutes*60;
-        this.seconds = currentSeconds;
+        calculateTime(currentSeconds, this);
     }
 
     public void tickDown() {
         int currentSeconds = getInSecond(this) - 1;
-        this.hours = (currentSeconds / 3600);
-        currentSeconds -= this.hours*3600;
-        this.hours %= 24;
-        this.minutes = currentSeconds / 60;
-        currentSeconds -= this.minutes*60;
-        this.seconds = currentSeconds;
+        calculateTime(currentSeconds, this);
     }
 
     public int getInSecond(Clock obj) {
         return (obj.getHours()*3600 + obj.getMinutes()*60 + obj.getSeconds());
+    }
+
+    public void calculateTime(int currentSeconds, Clock obj) {
+        obj.setHours(currentSeconds / 3600);
+        currentSeconds -= obj.getHours()*3600;
+        obj.setHours(getHours() % 24);
+        obj.setMinutes(currentSeconds / 60);
+        currentSeconds -= obj.getMinutes()*60;
+        obj.setSeconds(currentSeconds);
     }
 
     public String toString() {
